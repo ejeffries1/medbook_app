3e764ce45f5dbb90ccf2abed592182d56292edc7
evan <ejeffries14@gmail.com>
2021-12-09 19:35:26 -0600

    first commit

---
+++ lib/medbook_app.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "medbook_app/version"
+
+module MedbookApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/medbook_app/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MedbookApp
+  VERSION = "0.1.0"
+end
