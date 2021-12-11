0f7107241cb691c2a9997337fe7dafb2a3779db6
evan <ejeffries14@gmail.com>
2021-12-11 01:21:51 -0600

    patient table

--- bin/app/controllers/application_controller.rb
+++ bin/app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+    configure do
+        enable :sessions
+        set :session_secrets, "medbook_secret"
+    end
+
+
+    helpers do
+        def logged_in?
+            !!session[:user_id]
+        end
+
+        def current_user
+            Patient.find(session[:user_id])
+        end
+    end
 end
--- db/migrate/20211211050728_create_patients.rb
+++ db/migrate/20211211050728_create_patients.rb
@@ -1,4 +1,9 @@
 class CreatePatients < ActiveRecord::Migration[5.2]
   def change
+    create_table :patients do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+    end
   end
 end
